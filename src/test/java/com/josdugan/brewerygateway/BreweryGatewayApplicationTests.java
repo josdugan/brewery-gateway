89e901696b5aae72953d960646519503689d234c
josdugan <josdugan@gmail.com>
2022-06-30 13:36:35 -0400

    initial commit and route config

---
+++ src/test/java/com/josdugan/brewerygateway/BreweryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josdugan.brewerygateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreweryGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
