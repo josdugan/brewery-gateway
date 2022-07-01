5266d828fa8246edd5eab7a4983ac77feb2ca303
josdugan <josdugan@gmail.com>
2022-07-01 14:14:06 -0400

    Add eureka client; load-balanced config

--- src/main/java/com/josdugan/brewerygateway/config/LoadBalancedRoutesConfig.java
+++ src/main/java/com/josdugan/brewerygateway/config/LoadBalancedRoutesConfig.java
@@ -6,19 +6,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("!local-discovery")
+@Profile("local-discovery")
 @Configuration
-public class LocalHostRouteConfig {
+public class LoadBalancedRoutesConfig {
 
     @Bean
-    public RouteLocator localHostRoutes(RouteLocatorBuilder builder) {
+    public RouteLocator loadBalancedRoutes(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route("beer-service", r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
-                        .uri("http://localhost:8080"))
-                .route("beer-order-service", r -> r.path( "/api/v1/customers/**")
-                        .uri("http://localhost:8081"))
+                        .uri("lb://beer-service"))
+                .route("beer-order-service", r -> r.path("/api/v1/customers/**")
+                        .uri("lb://beer-order-service"))
                 .route("beer-inventory-service", r -> r.path("/api/v1/beer/*/inventory")
-                        .uri("http://localhost:8082"))
+                        .uri("lb://beer-inventory-service"))
                 .build();
     }
 }
